a5d67e137c3b44d7413bb19781c0a54633fe0661
TravisHuang459 <33121818+Jason16080329@users.noreply.github.com>
2020-09-06 21:12:56 +1000

    Tutorial for Week5

---
+++ Week5/W5ExeStudentWebservice/src/helloword/NameStorageBean.java
@@ -0,0 +1,23 @@
+package helloword;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+
+/**
+ * Session Bean implementation class NameStorageBean
+ */
+@Singleton
+@LocalBean
+public class NameStorageBean {
+
+	private String name = "World";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
